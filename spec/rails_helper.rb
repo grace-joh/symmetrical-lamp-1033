56150d9e734ffdd701dff7d0f70a8d0b57eeb862
Grace Joh <graceehjoh@gmail.com>
2023-04-10 10:49:10 -0500

    db: create seed and add test_data to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,6 +63,32 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  def test_data
+    @corner_market = Supermarket.create!(name: 'Corner Market', location: '123 Food St.')
+    @kroger = Supermarket.create!(name: 'Kroger', location: '123 Fresh Rd.')
+
+    @item1 = @corner_market.items.create!(name: 'Banana', price: 1)
+    @item2 = @corner_market.items.create!(name: 'Cereal', price: 4)
+    @item3 = @corner_market.items.create!(name: 'Juice', price: 2)
+    @item4 = @kroger.items.create!(name: 'Milk', price: 2)
+    @item5 = @kroger.items.create!(name: 'Eggs', price: 3)
+    @item6 = @kroger.items.create!(name: 'Hot Cheetos', price: 4)
+    @item7 = @kroger.items.create!(name: 'Ice Cream', price: 3)
+
+    @customer1 = Customer.create!(name: 'Sally Shopper')
+    @customer2 = Customer.create!(name: 'Sam Shopper')
+
+    @customer_item1 = CustomerItem.create!(customer: @customer1, item: @item1)
+    @customer_item2 = CustomerItem.create!(customer: @customer1, item: @item4)
+    @customer_item3 = CustomerItem.create!(customer: @customer1, item: @item5)
+    @customer_item4 = CustomerItem.create!(customer: @customer1, item: @item7)
+    @customer_item5 = CustomerItem.create!(customer: @customer2, item: @item4)
+    @customer_item6 = CustomerItem.create!(customer: @customer2, item: @item6)
+    @customer_item7 = CustomerItem.create!(customer: @customer2, item: @item7)
+    @customer_item8 = CustomerItem.create!(customer: @customer2, item: @item2)
+    @customer_item9 = CustomerItem.create!(customer: @customer1, item: @item3)
+  end
 end
 
 Shoulda::Matchers.configure do |config|

13e93a3983aa160677d830e09f20780c56f56f12
Grace Joh <graceehjoh@gmail.com>
2023-04-10 12:31:37 -0500

    Extension 1: Supermarket show page lists customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/supermarkets/:id', to: 'supermarkets#show'
+
+  get '/items', to: 'items#index'
+
   get '/customers/:id', to: 'customers#show'
 
   post 'customers/:id/items', to: 'customers/items#create'
-
-  get '/items', to: 'items#index'
 end

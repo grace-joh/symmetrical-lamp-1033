4457938bf0ea802badf25556bb334cb41de75989
Grace Joh <graceehjoh@gmail.com>
2023-04-10 11:11:09 -0500

    test: create model test for Item #supermarket_name

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Item, type: :model do
     it { should have_many :customer_items }
     it { should have_many(:customers).through(:customer_items) }
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      test_data
+    end
+
+    describe '#supermarket_name' do
+      it 'returns the name of the supermarket the item is at' do
+        expect(@item1.supermarket_name).to eq('Corner Market')
+        expect(@item7.supermarket_name).to eq('Kroger')
+      end
+    end
+  end
 end
